9d0713db1e53c8e0713b76a562dbc73950b65650
chrisnaadhi <chrisnaadhip@gmail.com>
2023-06-18 22:16:17 +0700

    add collection form

--- src/lib/components/items/FormCollection.svelte
+++ src/lib/components/items/FormCollection.svelte
@@ -9,16 +9,36 @@
 	<div>
 		<form action="/manage/collection/new" method="POST" use:enhance>
 			<div class="flex flex-col">
-				<label for="title">Title:</label>
+				<label for="subject">Subject Name:</label>
 				<input type="text" name="title" id="title" />
+			</div>
+			<div class="flex flex-col">
+				<label for="subject-slug">Subject Slug:</label>
+				<input type="text" name="slug" />
+			</div>
+			<div class="flex flex-col">
+				<label for="status">Status:</label>
+				<select name="status" id="status">
+					<option value="active">Active</option>
+					<option value="inactive">Inactive</option>
+					<option value="pending">Pending</option>
+				</select>
 			</div>
 			<div class="flex flex-col">
 				<label for="description">Description:</label>
 				<textarea name="description" id="description" />
 			</div>
-			<div class="flex gap-1 items-baseline">
-				<input type="checkbox" name="agreement" id="agreement" />
-				<p>Agree to Terms and Conditions</p>
+			<div class="flex flex-col">
+				<label for="keywords">Keywords:</label>
+				<input type="text" name="keywords" id="keywords" />
+			</div>
+			<div class="flex flex-col">
+				<label for="type">Subject Type:</label>
+				<select name="typesubject" id="typesubject">
+					<option value="guide">Guide</option>
+					<option value="course">Course</option>
+					<option value="topic">Topic</option>
+				</select>
 			</div>
 			<button type="submit" class="btn bg-violet-5 text-white">Submit</button>
 		</form>
@@ -27,7 +47,8 @@
 
 <style>
 	input,
-	textarea {
+	textarea,
+	select {
 		--at-apply: max-w-md p-2;
 	}
 
--- src/routes/(dashboard)/manage/collection/new/+page.server.ts
+++ src/routes/(dashboard)/manage/collection/new/+page.server.ts
@@ -1,14 +1,25 @@
 import type { Actions } from './$types';
 import { redirect } from '@sveltejs/kit';
+import { prisma } from '$lib/server/prisma';
 
 export const actions = {
 	default: async (event) => {
 		const data = await event.request.formData();
-		const title = data.get('title');
-		const description = data.get('description');
-		const agreement = data.get('agreement');
+		const subjectName = data.get('title');
+		const subjectSlug = data.get('slug');
+		const subjectStatus = data.get('status');
+		const subjectDescription = data.get('description');
+		const subjectKeywords = data.get('keywords');
+		const subjectType = data.get('typesubject');
 
-		console.log(title, description, agreement);
+		console.log(`
+		Subject Name: ${subjectName},
+		Subject Slug: ${subjectSlug},
+		Subject Status: ${subjectStatus},
+		Subject Description: ${subjectDescription},
+		Subject Keywords: ${subjectKeywords},
+		Subject Type: ${subjectType}
+		`);
 
 		throw redirect(302, '/manage/collection');
 	}
