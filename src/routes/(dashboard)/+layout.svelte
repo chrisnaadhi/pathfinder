910f0d5bbf7c71458d1bfe33e362b7e3dcfdb3f3
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2023-08-19 16:00:53 +0700

    fix layout icon

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -17,28 +17,28 @@
 					<div class="i-mdi-home w-5 h-5" />
 					<p>Dashboard</p>
 				</a>
-				<a href="/manage/collection" class="menu-item">
-					<div class="i-mdi-home w-5 h-5" />
-					Collection
-				</a>
 				<a href="/manage/profile" class="menu-item">
-					<div class="i-mdi-home w-5 h-5" />
+					<div class="i-mdi-account-edit w-5 h-5" />
 					Profile
 				</a>
-				<a href="/manage/#" class="menu-item">
-					<div class="i-mdi-home w-5 h-5" />
-					Guides
+				<a href="/manage/collection" class="menu-item">
+					<div class="i-mdi-bookshelf w-5 h-5" />
+					Subjects
 				</a>
 				<a href="/manage/member" class="menu-item">
-					<div class="i-mdi-home w-5 h-5" />
+					<div class="i-mdi-account-group w-5 h-5" />
 					Member
 				</a>
+				<a href="/manage/#" class="menu-item">
+					<div class="i-mdi-folder-information w-5 h-5" />
+					Guides
+				</a>
 				<a href="/manage/setting" class="menu-item">
-					<div class="i-mdi-home w-5 h-5" />
+					<div class="i-mdi-cog w-5 h-5" />
 					Setting
 				</a>
 			</div>
-			<div>
+			<div class="flex items-center justify-center">
 				<a href="/" class="btn bg-violet-5 text-white font-semibold">Exit Dashboard</a>
 			</div>
 		</div>
