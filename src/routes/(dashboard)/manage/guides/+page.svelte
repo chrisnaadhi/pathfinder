f502092ab365fd18653f56a3ab9cb936c5b0fe0a
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2023-11-08 14:05:50 +0700

    finishing guide

--- src/routes/(dashboard)/manage/guides/+page.svelte
+++ src/routes/(dashboard)/manage/guides/+page.svelte
@@ -1,5 +1,13 @@
-<script>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
 	let placeholder = 'Tata cara penggunaan Pathfinder';
+
+	let selectedTab = 'hierarki';
+
+	const selectTab = (value: string) => {
+		selectedTab = value;
+	};
 </script>
 
 <section>
@@ -9,86 +17,145 @@
 		exercitationem dignissimos necessitatibus consequatur vel amet blanditiis possimus quam illo
 		ratione laborum eius.
 	</p>
-	<h4 class="my-5">Hierarki Data dalam <span class="dfTx">Pathfinder</span></h4>
-	<div class="flex gap-4">
-		<div class="flex flex-col max-w-40 gap-1 text-center">
-			<div class="dfBg p-5 rounded">
-				<h5>Faculty</h5>
+
+	<section>
+		<button
+			class="btn"
+			class:active-btn={selectedTab === 'hierarki'}
+			on:click={() => selectTab('hierarki')}
+		>
+			<h6>Hierarki Data</h6>
+		</button>
+		<button
+			class="btn"
+			class:active-btn={selectedTab === 'workflow'}
+			on:click={() => selectTab('workflow')}
+		>
+			<h6>Workflow</h6>
+		</button>
+	</section>
+
+	{#if selectedTab === 'hierarki'}
+		<section transition:fade={{ delay: 150, duration: 300 }}>
+			<h4 class="my-5">Hierarki Data dalam <span class="dfTx">Pathfinder</span></h4>
+			<div class="flex flex-col gap-1">
+				<div class="flex">
+					<div class="text-center">
+						<div class="dfBg w-50 p-5 rounded">
+							<h5>Faculty</h5>
+						</div>
+						<div class="font-semibold text-5xl m-0">&DownArrow;</div>
+					</div>
+
+					<div class="px-5">
+						<h5 class="dfTx">Faculty</h5>
+						<p>
+							Merupakan komponen kelompok paling atas untuk menampung setiap disiplin keilmuan yang
+							berada dalam satu rumpun. Contohnya adalah
+							<span class="highlight"> Fakultas Ilmu Komunikasi </span> yang berada di Universitas Padjadjaran
+						</p>
+					</div>
+				</div>
+
+				<div class="flex">
+					<div class="text-center">
+						<div class="dfBg w-50 p-5 rounded">
+							<h5>Discipline</h5>
+						</div>
+						<div class="font-semibold text-5xl m-0">&DownArrow;</div>
+					</div>
+					<div class="px-5">
+						<h5 class="dfTx">Discipline</h5>
+						<p>
+							Merupakan komponen terkait disiplin keilmuan yang merujuk pada kumpulan subjek satu
+							rumpun. Disiplin merupakan konsentrasi studi yang merinci dan mengelompokkan berbagai
+							subjek dalam satu disiplin keilmuan. Contohnya adalah
+							<span class="highlight"> Ilmu Perpustakaan </span>
+							yang berada di Fakultas Ilmu Komunikasi Universitas Padjadjaran.
+						</p>
+					</div>
+				</div>
+
+				<div class="flex">
+					<div class="text-center">
+						<div class="dfBg w-50 p-5 rounded">
+							<h5>Subjects</h5>
+						</div>
+						<div class="font-semibold text-5xl m-0">&DownArrow;</div>
+					</div>
+					<div class="px-5">
+						<h5 class="dfTx">Subjects</h5>
+						<p>
+							Merupakan komponen kelompok dari Subjek sebuah keilmuan. Subjek ini merupakan salah
+							satu konsentrasi keilmuan dalam membahas topik tertentu dari sebuah disiplin ilmu.
+							Contohnya adalah
+							<span class="highlight">Klasifikasi</span> dalam Disiplin Ilmu Perpustakaan.
+						</p>
+					</div>
+				</div>
+
+				<div class="flex">
+					<div class="text-center">
+						<div class="dfBg w-50 p-5 rounded">
+							<h5>Collections</h5>
+						</div>
+						<div class="font-semibold text-5xl m-0">&DownArrow;</div>
+					</div>
+					<div class="px-5">
+						<h5 class="dfTx">Collections</h5>
+						<p>
+							Merupakan kumpulan koleksi yang ada dalam sebuah subjek. Koleksi bisa ditentukan dari
+							tipe masing-masing koleksi ataupun media format yang disajikan. Contohnya adalah <span
+								class="highlight">Koleksi Buku</span
+							> dalam Klasifikasi.
+						</p>
+					</div>
+				</div>
+
+				<div class="flex">
+					<div class="text-center">
+						<div class="dfBg w-50 p-5 rounded">
+							<h5>Contents</h5>
+						</div>
+					</div>
+					<div class="px-5">
+						<h5 class="dfTx">Contents</h5>
+						<p>
+							Contents merupakan salah satu koleksi yang ada dalam kumpulan koleksi di Subjek.
+							Contents dapat berisi satu atau lebih informasi mengenai koleksi yang ada di
+							masing-masing Subjek. Contohnya adalah <span class="highlight"
+								>Buku E-DDC Edisi 23</span
+							> yang berada dalam Koleksi Buku Fisik dari Klasifikasi.
+						</p>
+					</div>
+				</div>
 			</div>
-			<div class="font-semibold text-5xl m-0">&DownArrow;</div>
-			<div class="dfBg p-5 rounded">
-				<h5>Discipline</h5>
+		</section>
+	{:else if selectedTab === 'workflow'}
+		<section transition:fade={{ delay: 150, duration: 300 }}>
+			<h4 class="my-4">Workflow <span class="dfTx">Pathfinder</span></h4>
+			<p>
+				Alur Kerja dari penyediaan Subjek dan Konten yang ada dalam Sistem Lantera Pathfinder. Alur
+				ini diperuntukkan untuk Koordinator Subjek dan Subjek Spesialis, setiap langkah didasarkan
+				pada hierarki data yang sudah tersedia dalam sistem. Urutan pembuatan konten dan data subjek
+				yang dilakukan oleh sistem Pathfinder diurutkan oleh masing-masing subjek spesialis dan
+				dilakukan secara berurutan dengan memperhatikan setiap langkah yang tersedia dalam gambar
+				berikut.
+			</p>
+			<div class="my-5">
+				<img src="/img/workflow.webp" alt="Workflow of Pathfinder" />
 			</div>
-			<div class="font-semibold text-5xl m-0">&DownArrow;</div>
-			<div class="dfBg p-5 rounded">
-				<h5>Subjects</h5>
-			</div>
-			<div class="font-semibold text-5xl m-0">&DownArrow;</div>
-			<div class="dfBg p-5 rounded">
-				<h5>Collections</h5>
-			</div>
-			<div class="font-semibold text-5xl m-0">&DownArrow;</div>
-			<div class="dfBg p-5 rounded">
-				<h5>Contents</h5>
-			</div>
-		</div>
-		<div class="w-full">
-			<div class="px-5">
-				<h5 class="dfTx">Faculty</h5>
-				<p>
-					Merupakan komponen kelompok paling atas untuk menampung setiap disiplin keilmuan yang
-					berada dalam satu rumpun. Contohnya adalah Fakultas Ilmu Komunikasi yang berada di
-					Universitas Padjadjaran
-				</p>
-			</div>
-			<div class="font-semibold text-4xl m-0 text-transparent">&UpArrow;</div>
-			<div class="px-5">
-				<h5 class="dfTx">Discipline</h5>
-				<p>
-					Merupakan komponen terkait disiplin keilmuan yang merujuk pada kumpulan subjek satu
-					rumpun. Disiplin merupakan konsentrasi studi yang merinci dan mengelompokkan berbagai
-					subjek dalam satu disiplin keilmuan. Contohnya adalah <span class="highlight"
-						>Ilmu Perpustakaan</span
-					>.
-				</p>
-			</div>
-			<div class="font-semibold text-5xl m-0 text-transparent">&UpArrow;</div>
-			<div class="px-5">
-				<h5 class="dfTx">Subjects</h5>
-				<p>
-					Merupakan komponen kelompok dari Subjek sebuah keilmuan. Subjek ini merupakan salah satu
-					konsentrasi keilmuan dalam membahas topik tertentu dari sebuah disiplin ilmu. Contohnya
-					adalah
-					<span class="highlight">Klasifikasi</span> dalam Disiplin Ilmu Perpustakaan.
-				</p>
-			</div>
-			<div class="font-semibold text-5xl m-0 text-transparent">&UpArrow;</div>
-			<div class="px-5">
-				<h5 class="dfTx">Collections</h5>
-				<p>
-					Merupakan kumpulan koleksi yang ada dalam sebuah subjek. Koleksi bisa ditentukan dari tipe
-					masing-masing koleksi ataupun media format yang disajikan. Contohnya adalah <span
-						class="highlight">Koleksi Buku</span
-					> dalam Klasifikasi.
-				</p>
-			</div>
-			<div class="font-semibold text-5xl m-0 text-transparent">&UpArrow;</div>
-			<div class="px-5">
-				<h5 class="dfTx">Contents</h5>
-				<p>
-					Contents merupakan salah satu koleksi yang ada dalam kumpulan koleksi di Subjek. Contents
-					dapat berisi satu atau lebih informasi mengenai koleksi yang ada di masing-masing Subjek.
-					Contohnya adalah <span class="highlight">Buku E-DDC Edisi 23</span> yang berada dalam Koleksi
-					Buku Fisik dari Klasifikasi.
-				</p>
-			</div>
-		</div>
-	</div>
+		</section>
+	{/if}
 </section>
 
 <style>
 	p {
 		--at-apply: italic;
+	}
+
+	.active-btn {
+		--at-apply: dfBg;
 	}
 
 	.highlight {
